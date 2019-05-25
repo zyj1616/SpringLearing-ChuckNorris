1039a7252fb4ca5d86ad13b5e0fbd7da9393f273
y1zhou <y1zhou@ucsd.edu>
2019-05-24 17:49:21 -0700

    "Java based configuration"

---
+++ src/main/java/springframework/jokes/config/JokesConfiguration.java
@@ -0,0 +1,13 @@
+package springframework.jokes.config;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokesConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
